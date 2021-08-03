261fae4856d7c08addd8a2317443c3e39271fb19
Mac_Alan <15863465252@163.com>
2021-08-03 21:13:15 +0800

    update code

--- Self Attention/self attention.py
+++ Self Attention/self attention.py
@@ -5,8 +5,13 @@ import torch.nn as nn
 import os
 import torch.nn.functional as F
 
+'''
 # 代码的注释是在input.shape = (8,64,32,32)的基础上进行注释的
-
+query.view就是把长、宽像素拉直成一个维度，维度的维度值为原来的w * h
+key.view就是把长、宽像素拉直成一个维度，维度的维度值为原来的w * h  
+query和key正好是可以点乘的矩阵
+经过softmax会给每一个像素分配一个权重值，每一行的权重之和为1
+'''
 class Self_Attention(nn.Module):
     # in_dim为输入、输出特征的通道维度数
     def __init__(self,in_dim,activation):
@@ -19,6 +24,7 @@ class Self_Attention(nn.Module):
         self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
         self.gamma = nn.Parameter(torch.zeros(1))
 
+
         self.softmax  = nn.Softmax(dim=-1)
     def forward(self,x):
         """
@@ -29,9 +35,11 @@ class Self_Attention(nn.Module):
                 attention: B * N * N (N is Width*Height)
         """
         m_batchsize,C,width ,height = x.size()
-        # proj_query.shape = torch.Size([8, 1024, 8])
+        # proj_query.view.shape = torch.Size([8, 1024, 8])
+        # self.query_conv(x).shape = torch.Size([8, 32, 32, 32])
         proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B*N*C
-        # proj_key.shape = torch.Size([8, 8, 1024])
+        # proj_key.view.shape = torch.Size([8, 8, 1024])
+        # self.key_conv(x).shape = torch.Size([8, 32, 32, 32])
         proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B*C*N
         # energy.shape = torch.Size([8, 1024, 1024])
         energy =  torch.bmm(proj_query,proj_key) # batch的matmul B*N*N
